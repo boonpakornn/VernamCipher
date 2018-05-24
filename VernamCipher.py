eaa30948651d360d157bc63fa521c6e3905f441f
Boonbpknz <37540958+Boonbpknz@users.noreply.github.com>
2018-05-24 07:02:01 +0700

    1st Ver

--- VernamCipher.py
+++ VernamCipher.py
@@ -10,10 +10,11 @@ if(len(plainText) != len(key)):
     print "Invalid key, application terminated.";
 
 size = len(plainText);
+threshold = len(avaliableChar);
 
-plainValue = [size];
-keyValue = [size];
-cipherValue = [size];
+plainValue = [];
+keyValue = [];
+cipherValue = [];
 Cipher = '';
 
 
@@ -29,24 +30,25 @@ def CharValue(x):
 
 for i in range(0, size):
     num = numValue(plainText[i])
-    plainValue[i] = num;
+    plainValue.append(num);
 
 for i in range(0, size):
     num = numValue(key[i])
-    keyValue[i] = num;
-
-for i in range(0,size) :
-    print plainValue[i];
-
+    keyValue.append(num);
 
 for i in range(0, size):
-    cipherValue[i] = plainValue[i] + keyValue[i];
-    if cipherValue[i] > 25:
-        cipherValue[i] -= 26;
+    plainval = plainValue[i];
+    keyval = keyValue[i];
+    cipherval = plainval+keyval;
+
+    if cipherval > threshold-1:
+       cipherval -= threshold;
+    cipherValue.append(cipherval);
 
 for i in range(0, size):
     Cipher += CharValue(cipherValue[i]);
 
+print 'CipherText is : '
 print Cipher;
 
 
